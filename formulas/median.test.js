b51cf3a672a7bf7f333fd92c7fd870a75ae3e8d6
Suissinha <jnascimento@gmail.com>
2023-07-30 15:26:42 -0300

    Traduzindo

--- formulas/median.test.js
+++ formulas/median.test.js
@@ -1,39 +1,38 @@
 const { expect } = require('chai');
 const median = require('./median');
 
-const teamA = [38, 73, 86, 90, 111, 124]
-const teamB = [71, 84, 85, 89, 90, 103]
-const teamC = [59, 70, 77, 88, 95, 229]
+const teamA = [38, 73, 86, 90, 111, 124];
+const teamB = [71, 84, 85, 89, 90, 103];
+const teamC = [59, 70, 77, 88, 95, 229];
 
-describe('median', () => {
-  it('should return the median of an array with odd length', () => {
+describe('mediana', () => {
+  it('deve retornar a mediana de um array com tamanho ímpar', () => {
     const data = [1, 2, 3, 4, 5];
 
     expect(median(data)).equal(3);
   });
 
-  it('should return the median of an array with even length', () => {
+  it('deve retornar a mediana de um array com tamanho par', () => {
     const data = [1, 2, 3, 4];
 
     expect(median(data)).equal(2.5);
   });
 
-  it('should return the median of an empty array', () => {
+  it('deve retornar undefined para um array vazio', () => {
     const data = [];
 
     expect(median(data)).equal(undefined);
   });
 
-
-  it('should return the median of teamA', () => {
+  it('deve retornar a mediana da equipe A', () => {
     expect(median(teamA)).equal(88);
   });
 
-  it('should return the median of teamB', () => {
+  it('deve retornar a mediana da equipe B', () => {
     expect(median(teamB)).equal(87);
   });
 
-  it('should return the median of teamC', () => {
+  it('deve retornar a mediana da equipe C', () => {
     expect(median(teamC)).equal(82.5);
   });
 });
