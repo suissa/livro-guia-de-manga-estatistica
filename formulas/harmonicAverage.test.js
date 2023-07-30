3ac46d67e27a3865ac640d432259e0c9647d2a27
Suissinha <jnascimento@gmail.com>
2023-07-30 15:22:01 -0300

    Traduzindo

--- formulas/harmonicAverage.test.js
+++ formulas/harmonicAverage.test.js
@@ -2,19 +2,19 @@ const expect = require('chai').expect;
 const harmonicAverage = require('./harmonicAverage');
 
 describe('harmonicAverage', () => {
-  it('should return the correct harmonic average for an array of positive numbers', () => {
+  it('deve retornar a média harmônica correta para um array de números positivos', () => {
     expect(harmonicAverage([1, 2, 3])).to.equal(1.6363636363636365);
   });
 
-  it('should return the correct harmonic average for an array with zero', () => {
+  it('deve retornar a média harmônica correta para um array que contenha o número zero', () => {
     expect(harmonicAverage([1, 0, 3])).to.equal(0);
   });
 
-  it('should return Infinity for an array of all zeros', () => {
+  it('deve retornar Infinity para um array composto apenas por zeros', () => {
     expect(harmonicAverage([0, 0, 0])).to.equal(Infinity);
   });
 
-  it('should return Infinity for an empty array', () => {
+  it('deve retornar Infinity para um array vazio', () => {
     expect(harmonicAverage([])).to.equal(Infinity);
   });
 });
