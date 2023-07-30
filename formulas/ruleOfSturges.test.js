bbf211b192a9d308ce146b2446349351163ce0c5
Suissinha <jnascimento@gmail.com>
2023-07-30 15:23:41 -0300

    Traduzindo

--- formulas/ruleOfSturges.test.js
+++ formulas/ruleOfSturges.test.js
@@ -1,47 +1,34 @@
 const { expect } = require('chai');
 const ruleOfSturges = require('./ruleOfSturges');
 
-describe('rule of Sturges', () => {
-  it('should return 5 for length 20', () => {
-    
-    const data = Array.from({length: 20}, () => Math.floor(Math.random() * 1000));
-
+describe('regra de Sturges', () => {
+  it('deve retornar 5 para um comprimento de 20', () => {
+    const data = Array.from({ length: 20 }, () => Math.floor(Math.random() * 1000));
     expect(ruleOfSturges(data)).equal(5);
   });
-  it('should return 6 for length 25', () => {
-    
-    const data = Array.from({length: 25}, () => Math.floor(Math.random() * 1000));
 
+  it('deve retornar 6 para um comprimento de 25', () => {
+    const data = Array.from({ length: 25 }, () => Math.floor(Math.random() * 1000));
     expect(ruleOfSturges(data)).equal(6);
   });
 
-  it('should return 7 for length 50', () => {
-    
-    const data = Array.from({length: 50}, () => Math.floor(Math.random() * 1000));
-
+  it('deve retornar 7 para um comprimento de 50', () => {
+    const data = Array.from({ length: 50 }, () => Math.floor(Math.random() * 1000));
     expect(ruleOfSturges(data)).equal(7);
   });
 
-  it('should return 9 for length 250', () => {
-    
-    const data = Array.from({length: 250}, () => Math.floor(Math.random() * 1000));
-
+  it('deve retornar 9 para um comprimento de 250', () => {
+    const data = Array.from({ length: 250 }, () => Math.floor(Math.random() * 1000));
     expect(ruleOfSturges(data)).equal(9);
   });
 
-
-  it('should return 10 for length 500', () => {
-    
-    const data = Array.from({length: 500}, () => Math.floor(Math.random() * 1000));
-
+  it('deve retornar 10 para um comprimento de 500', () => {
+    const data = Array.from({ length: 500 }, () => Math.floor(Math.random() * 1000));
     expect(ruleOfSturges(data)).equal(10);
   });
 
-  it('should return 11 for length 1000', () => {
-    
-    const data = Array.from({length: 1000}, () => Math.floor(Math.random() * 1000));
-
+  it('deve retornar 11 para um comprimento de 1000', () => {
+    const data = Array.from({ length: 1000 }, () => Math.floor(Math.random() * 1000));
     expect(ruleOfSturges(data)).equal(11);
   });
-
 });
